918fb60f02ceb9792d13d0d61dd498772d917354
Osipova Daria <dariaaa511@yandex.ru>
2023-01-10 12:11:10 +0300

    another changes

--- scripts/index.js
+++ scripts/index.js
@@ -3,13 +3,13 @@ const popup = document.querySelector('.popup');
 const buttonClose = document.querySelector('.popup__close-button');
 const profileName = document.querySelector('.profile__name');
 const profileJob = document.querySelector('.profile__description');
-const editForm = document.querySelector('.popup__container');
+const editForm = document.querySelector('.popup__form');
 const nameInput = document.querySelector('.popup__text_type_name');
 const jobInput = document.querySelector('.popup__text_type_about');
 // let popupSumbitButton = document.querySelector(".popup__submit-btn");
 
 function openPopup() {
-  popup.classList.add('popup__container_opened');
+  popup.classList.add('popup_opened');
   nameInput.value = profileName.textContent;
   jobInput.value = profileJob.textContent;
 }
@@ -17,7 +17,7 @@ function openPopup() {
 buttonEdit.addEventListener('click', openPopup);
 
 function closePopup() {
-  popup.classList.remove('popup__container_opened');
+  popup.classList.remove('popup_opened');
 }
 
 buttonClose.addEventListener('click', closePopup);
